6e13ab976549d8fa8f0b0dfefa9e525ff3708b89
Takehiro-Matsuo <g1545367@cc.kyoto-su.ac.jp>
2017-07-07 08:08:26 +0900

    send.php, thanks.htmlの追加とtopのアニメーションの追加

---
+++ index.php
@@ -0,0 +1,321 @@
+<!DOCTYPE html>
+<html lang="ja" dir="ltr">
+<head>
+  <meta charset="utf-8">
+  <meta name="keywords" content="京都,家事代行,介護,さぽかじ,求人">
+  <meta name="description" content="京都市の家事代行『さぽかじ』では、介護や子育て、お仕事でお忙しい方々をサポートする家事代行のサービスをご提供しています。京都市内で家事代行をお探しの方はお気軽にご相談下さいませ。また、家事代行スタッフの求人募集も随時承っております。">
+  <meta name="viewport" content="width=device-width,user-scalable=no,initial-scale=1,maximum-scale=1.0">
+  <title>ご依頼申込み｜京都市の家事代行「さぽかじ」家事や介護のサポート</title>
+  <link rel="shortcut icon" href="../img/favicon.ico">
+  <link rel="apple-touch-icon" href="../images/apple-touch-icon.png">
+  <link href="../css/default.css" rel="stylesheet" media="all">
+  <link href="../css/common.css" rel="stylesheet" media="all">
+
+  <link rel="stylesheet" type="text/css" href="css/import.css" />
+  <link rel="stylesheet" type="text/css" href="css/mailform.css" />
+
+  <!--[if IE]>
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <![endif]-->
+  <!--[if lt IE 9]>
+  <script src="../js/html5shiv-printshiv.js"></script>
+  <script src="http://ie7-js.googlecode.com/svn/version/2.1(beta4)/IE9.js"></script>
+  <![endif]-->
+  <script src="http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"></script>
+  <script src="../js/css3-mediaqueries.js"></script>
+  <!--[if IE 8]><html class="ie8"><![endif]-->
+
+  <!-- カレンダー入力用ここから -->
+  <script src="http://ajax.googleapis.com/ajax/libs/jqueryui/1/jquery-ui.min.js"></script>
+  <script src="http://ajax.googleapis.com/ajax/libs/jqueryui/1/i18n/jquery.ui.datepicker-ja.min.js"></script>
+  <link rel="stylesheet" href="http://ajax.googleapis.com/ajax/libs/jqueryui/1/themes/redmond/jquery-ui.css" >
+  <script>
+  $(function() {
+    $.datepicker.setDefaults( $.datepicker.regional[ "ja" ] );
+    $( "#datepicker" ).datepicker();
+  });
+  </script>
+  <!-- カレンダー入力用ここまで -->
+
+  <script src="../js/google.js" type="text/javascript"></script>
+</head>
+<body>
+
+  <div id="wrapper">
+
+    <!-- #header -->
+    <header id="header">
+      <div class="logo"><a href="../index.php"><img src="../img/logos.png" alt="京都の家事代行「さぽかじ」"/></a></div>
+      <h1>さぽかじ ~安心・安全・リーズナブルな家事代行~</h1>
+      <div class="headnavi">
+        <a href="../order"><img src="../img/bt_head1.png" alt="ご依頼申込み"/></a>
+        <a href="../pay"><img src="../img/menu.png" alt="お支払"/></a>
+      </div>
+    </header>
+    <!-- #header END -->
+
+
+    <!-- #topix END -->
+    <div id="topix">
+
+      <!-- #nav -->
+      <nav id="nav">
+        <ul>
+          <li class="nav01"><a href="../index.php">ホーム</a></li>
+          <li class="nav02"><a href="../flow.html">ご利用の流れ</a></li>
+          <li class="nav03"><a href="../order">ご依頼申込み</a></li>
+          <li class="nav04"><a href="../pay">お支払い</a></li>
+          <li class="nav05"><a href="../contact">お問い合せ</a></li>
+          <li class="nav06"><a href="../recruit.html">求人情報</a></li>
+        </ul>
+      </nav>
+      <!-- #nav END -->
+
+    </div>
+    <!-- #topix END -->
+
+
+    <!-- #contents END -->
+    <div id="contents">
+
+      <section class="contactform">
+        <div class="stage">
+
+          <h2>ご依頼・お申込みフォーム</h2>
+          <form action="regist.php" method="POST" id="mail_form">
+            <dl>
+              <dt><span class="required">必須</span>名前<span>Your Name</span></dt>
+              <dd class="required"><input type="text" id="name_1" name="name_1" value="" /> <input type="text" id="name_2" name="name_2" value="" /></dd>
+
+              <dt><span class="required">必須</span>ふりがな<span>Name Reading</span></dt>
+              <dd><input type="text" id="read_1" name="read_1" value="" /> <input type="text" id="read_2" name="read_2" value="" /></dd>
+
+              <dt><span class="required">必須</span>メールアドレス<span>Mail Address</span></dt>
+              <dd class="required"><input type="text" id="mail_address" name="mail_address" value="" /></dd>
+
+              <dt><span class="required">必須</span>メールアドレス(確認)<span>Mail Address Confirm</span></dt>
+              <dd class="required"><input type="text" id="mail_address_confirm" name="mail_address_confirm" value="" /></dd>
+
+              <dt><span class="optional">任意</span>生年月日<span>Birth Day</span></dt>
+              <dd>西暦<select name="age_y" id="age_y">
+                <option value="1900">1900</option>
+                <option value="1901">1901</option>
+                <option value="1902">1902</option>
+                <option value="1903">1903</option>
+                <option value="1904">1904</option>
+                <option value="1905">1905</option>
+                <option value="1906">1906</option>
+                <option value="1907">1907</option>
+                <option value="1908">1908</option>
+                <option value="1909">1909</option>
+                <option value="1910">1910</option>
+                <option value="1911">1911</option>
+                <option value="1912">1912</option>
+                <option value="1913">1913</option>
+                <option value="1914">1914</option>
+                <option value="1915">1915</option>
+                <option value="1916">1916</option>
+                <option value="1917">1917</option>
+                <option value="1918">1918</option>
+                <option value="1919">1919</option>
+                <option value="1920">1920</option>
+                <option value="1921">1921</option>
+                <option value="1922">1922</option>
+                <option value="1923">1923</option>
+                <option value="1924">1924</option>
+                <option value="1925">1925</option>
+                <option value="1926">1926</option>
+                <option value="1927">1927</option>
+                <option value="1928">1928</option>
+                <option value="1929">1929</option>
+                <option value="1930">1930</option>
+                <option value="1931">1931</option>
+                <option value="1932">1932</option>
+                <option value="1933">1933</option>
+                <option value="1934">1934</option>
+                <option value="1935">1935</option>
+                <option value="1936">1936</option>
+                <option value="1937">1937</option>
+                <option value="1938">1938</option>
+                <option value="1939">1939</option>
+                <option value="1940">1940</option>
+                <option value="1941">1941</option>
+                <option value="1942">1942</option>
+                <option value="1943">1943</option>
+                <option value="1944">1944</option>
+                <option value="1945">1945</option>
+                <option value="1946">1946</option>
+                <option value="1947">1947</option>
+                <option value="1948">1948</option>
+                <option value="1949">1949</option>
+                <option value="1950">1950</option>
+                <option value="1951">1951</option>
+                <option value="1952">1952</option>
+                <option value="1953">1953</option>
+                <option value="1954">1954</option>
+                <option value="1955">1955</option>
+                <option value="1956">1956</option>
+                <option value="1957">1957</option>
+                <option value="1958">1958</option>
+                <option value="1959">1959</option>
+                <option value="1960">1960</option>
+                <option value="1961">1961</option>
+                <option value="1962">1962</option>
+                <option value="1963">1963</option>
+                <option value="1964">1964</option>
+                <option value="1965">1965</option>
+                <option value="1966">1966</option>
+                <option value="1967">1967</option>
+                <option value="1968">1968</option>
+                <option value="1969">1969</option>
+                <option value="1970">1970</option>
+                <option value="1971">1971</option>
+                <option value="1972">1972</option>
+                <option value="1973">1973</option>
+                <option value="1974">1974</option>
+                <option value="1975">1975</option>
+                <option value="1976">1976</option>
+                <option value="1977">1977</option>
+                <option value="1978">1978</option>
+                <option value="1979">1979</option>
+                <option value="1980" selected="selected">1980</option>
+                <option value="1981">1981</option>
+                <option value="1982">1982</option>
+                <option value="1983">1983</option>
+                <option value="1984">1984</option>
+                <option value="1985">1985</option>
+                <option value="1986">1986</option>
+                <option value="1987">1987</option>
+                <option value="1988">1988</option>
+                <option value="1989">1989</option>
+                <option value="1990">1990</option>
+                <option value="1991">1991</option>
+                <option value="1992">1992</option>
+                <option value="1993">1993</option>
+                <option value="1994">1994</option>
+                <option value="1995">1995</option>
+                <option value="1996">1996</option>
+                <option value="1997">1997</option>
+                <option value="1998">1998</option>
+                <option value="1999">1999</option>
+                <option value="2000">2000</option>
+                <option value="2001">2001</option>
+                <option value="2002">2002</option>
+                <option value="2003">2003</option>
+                <option value="2004">2004</option>
+                <option value="2005">2005</option>
+                <option value="2006">2006</option>
+                <option value="2007">2007</option>
+                <option value="2008">2008</option>
+                <option value="2009">2009</option>
+                <option value="2010">2010</option>
+                <option value="2011">2011</option>
+                <option value="2012">2012</option>
+                <option value="2013">2013</option>
+                <option value="2014">2014</option>
+                <option value="2015">2015</option>
+                <option value="2016">2016</option>
+              </select>
+              年
+              <select name="age_m" id="age_m">
+                <option value="1">1</option>
+                <option value="2">2</option>
+                <option value="3">3</option>
+                <option value="4">4</option>
+                <option value="5">5</option>
+                <option value="6">6</option>
+                <option value="7">7</option>
+                <option value="8">8</option>
+                <option value="9">9</option>
+                <option value="10">10</option>
+                <option value="11">11</option>
+                <option value="12">12</option>
+              </select>
+              月
+              <select name="age_d" id="age_d">
+                <option value="1">1</option>
+                <option value="2">2</option>
+                <option value="3">3</option>
+                <option value="4">4</option>
+                <option value="5">5</option>
+                <option value="6">6</option>
+                <option value="7">7</option>
+                <option value="8">8</option>
+                <option value="9">9</option>
+                <option value="10">10</option>
+                <option value="11">11</option>
+                <option value="12">12</option>
+                <option value="13">13</option>
+                <option value="14">14</option>
+                <option value="15">15</option>
+                <option value="16">16</option>
+                <option value="17">17</option>
+                <option value="18">18</option>
+                <option value="19">19</option>
+                <option value="20">20</option>
+                <option value="21">21</option>
+                <option value="22">22</option>
+                <option value="23">23</option>
+                <option value="24">24</option>
+                <option value="25">25</option>
+                <option value="26">26</option>
+                <option value="27">27</option>
+                <option value="28">28</option>
+                <option value="29">29</option>
+                <option value="30">30</option>
+                <option value="31">31</option>
+              </select>
+              日
+            </dd>
+
+            <dt><span class="required">必須</span>郵便番号<span>Postal</span></dt>
+            <dd><input type="text" id="postal" name="postal" value="" onkeyup="AjaxZip3.zip2addr(this,'','address_1','address_1');" />　<a href="http://www.post.japanpost.jp/zipcode/" target="_blank">郵便番号検索</a></dd>
+
+            <dt><span class="required">必須</span>住所<span>Address</span></dt>
+            <dd><input type="text" id="address_1" name="address_1" value="" /><input type="text" id="address_2" name="address_2" value="" /></dd>
+
+            <dt><span class="required">必須</span>電話番号<span>Phone Number</span></dt>
+            <dd class="required"><input type="text" id="phone" name="phone" value="" /></dd>
+
+            <dt><span class="required">必須</span>初回訪問の希望日時<span>Day</span></dt>
+            <dd class="required">
+                <input type="date" id="day" name="day" value="" class="hasDatepicker"/>
+                <img class="ui-datepicker-trigger" src="image/ico_calendar.png" alt="..." title="...">
+            </dd>
+
+            <dt><span class="required">必須</span>ご希望コース<span>Service Menu</span></dt>
+            <dd class="required">
+              <ul>
+                <li><label><input type="radio" id="kind_1" name="service" value="2時間コース" checked>2時間コース</label></li>
+                <li><label><input type="radio" id="kind_2" name="service" value="2.5時間コース">2.5時間コース</label></li>
+                <li><label><input type="radio" id="kind_3" name="service" value="3時間コース">3時間コース</label></li>
+              </ul>
+              <span></span></dd>
+
+              <dt><span class="optional">任意</span>料理オプション<span>Cooking Option</span></dt>
+              <dd>
+                <ul>
+                  <li><label><input type="checkbox" id="cooking" name="cooking" value="料理オプション付き">料理オプションを申し込む</label></li>
+                </ul>
+              </dd>
+
+              <dt><span class="optional">任意</span>不在業務のご希望<span>Absence Service</span></dt>
+              <dd>
+                <ul>
+                  <li><label><input type="checkbox" id="absent" name="absent" value="不在業務を希望する">
+                    お客様不在時の業務を希望する</label></li>
+                  </ul>
+              </dd>
+
+              <dt><span class="required">必須</span>ご依頼内容の詳細 <span>Detail</span></dt>
+              <dd class="required"><textarea id="detail" name="detail" cols="40" rows="5" placeholder="ご希望の家事内容、お掃除の場合はご希望の部屋（例：キッチン・風呂・LDK12畳など）を詳しくご記入下さい"></textarea><span></span></dd>
+
+              <dt style="width: 100%; text-align: left;">〈お客様にご用意をお願いしているもの〉<br>
+              ・掃除機。・雑巾2〜3枚。・古い⻭ブラシ。・古新聞。・各専用洗剤。・風呂用スリッパ。</dt>
+              <dt style="border:none;">送信する<span>Send</span></dt>
+              <dd class="required" style="border:none;"><input type="submit" id="mail_submit_button" name="mail_submit_button" value="送信する"></dd>
+              </dl>
+            </form>
+
+          </body>
+          </html>
