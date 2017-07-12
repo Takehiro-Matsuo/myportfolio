2995b2a193195134d37e60918b0b36aad09447b5
Takehiro-Matsuo <g1545367@cc.kyoto-su.ac.jp>
2017-07-12 17:16:09 +0900

    テーマ化修正

---
+++ footer.php
@@ -0,0 +1,10 @@
+<script>
+$('.headerNaviToggle').click(function(){//headerのnaviを開くアニメーション
+  $('#header').toggleClass('openHeaderNavi');
+});
+</script>
+<script>new WOW().init();//画面スクロール時のアニメーションのトリガー</script>
+
+</body>
+</html>
+<?php wp_footer(); ?>
